b458a3074acdcd120cc8d47ffe6191627cd5ae09
anuseth12 <anuseth17@gmail.com>
2023-04-08 21:20:03 +0530

    second commit

--- src/test/java/com/epoccoverter/testcases/TC01_GetRequest.java
+++ src/test/java/com/epoccoverter/testcases/TC01_GetRequest.java
@@ -1,98 +1,167 @@
 package com.epoccoverter.testcases;
 
-import org.json.JSONArray;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
+import com.epocconverter.responsevalidation.Responsevalidation;
+import com.APIEpocCovereter.teststeps.httpmethods;
+import com.epocconverter.utilies.*;
 
+import org.apache.http.protocol.HTTP;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import static io.restassured.RestAssured.*;
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertTrue;
-
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Date;
 
 import io.restassured.response.Response;
-import io.restassured.matcher.RestAssuredMatchers.*;
-import io.restassured.path.json.JsonPath;
-import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
-import static org.hamcrest.Matchers.*;
 
-public class TC01_GetRequest {
+public class TC01_Getrequest {
+	
+	
+	public Logger logger;// for logs
+	//logs
+	
+			
+	@Test
+	public void testcase1_get() throws IOException
+	{
+		logger= LogManager.getLogger(this.getClass());
+		logger.info("********** Loading properties file  ***************");
+		Properties pr=LoadProperties.loadproperties1("../APIEpocConverter/URI.Properties");
+	
+		
+		httpmethods http=new httpmethods(pr);
+		System.out.println("******************Test case 1 for valid timestamp***************************");
+		
+		Response res=http.getrequest("QA_URI");
+		
+	   logger.info("**********Get Request is completed***************");
+	   Responsevalidation.statuscodevalidation(res, 200);
+	   Responsevalidation.datevalidation(1451613802);
+	
+	      
+		
+}
+	
+	
+     @Test
+          public void testcase2_invalidtimestamp() throws IOException
+      {
+	logger= LogManager.getLogger(this.getClass());
+	Properties pr=LoadProperties.loadproperties1("../APIEpocConverter/URI.Properties");
+	
+	
+	httpmethods http=new httpmethods(pr);
+	System.out.println("******************Test case 2 for invalid timestamp***************************");
+	
+	Response res=http.invalid_getrequest("QA_InvalidURI");
+	//Response res=http.getrequest_invalid("Base_url","asdfasd");
+	
+	logger.info("**********Get Request is completed***************");
+   Responsevalidation.statuscodevalidation(res, 200);
+   Assert.assertEquals(res.asString(), "false");
+   
+ 
+      
+	
+}
 
+	@Test
+	public void testcase3_get() throws IOException
+	{
+		logger= LogManager.getLogger(this.getClass());
+		logger.info("********** Loading properties file  ***************");
+		Properties pr=LoadProperties.loadproperties1("../APIEpocConverter/URI.Properties");
+		
+		
+		httpmethods http=new httpmethods(pr);
+		System.out.println("******************Test case 3 for timestamp beyond year 2038***************************");
+		
+		
+		Response res=http.getrequest("QA_future");
+		//Response res1=http.invalid_getrequest("QA_URL",);
+		logger.info("**********Get Request is completed***************");
+	   Responsevalidation.statuscodevalidation(res, 200);
+	   Responsevalidation.datevalidation(2147483647);
+		logger.info("**********Date validation is completed***************");
+      }
 	
 	
 	@Test
-	
-	  public void testUnixTimestampConverterAPI()
+	public void testcase4_get() throws IOException
 	{
-		Response res=
-				given()
-			//.pathParam("timestamp", "unix-timestamp-converter")
-			.queryParam("cached","true")
-			.queryParam("s", "1451613802")
-				
-			
-		  
-      .when()
-          .get("https://helloacm.com/api/unix-timestamp-converter/");
-
-          int statusCode = res.getStatusCode();
-          System.out.println("Response status code: " + statusCode);
-          
-
-          String responseBody = res.getBody().asString();
-       
-
-          System.out.println("Response body: " + responseBody);
-        //  Assert.assertNotNull(responseBody);
-         Assert.assertTrue(responseBody.contains("2016-01-01"));
-      //  Date date= UnixTimestampConverterTest.testcovert(1451613802);
-          Assert.assertTrue(responseBody.contains("false"));
-          
-          //https://helloacm.com/api/unix-timestamp-converter/?cached&s=1648752000
+		logger= LogManager.getLogger(this.getClass());
+		Properties pr=LoadProperties.loadproperties1("../APIEpocConverter/URI.Properties");
+		
+		
+		httpmethods http=new httpmethods(pr);
+		System.out.println("******************Test case 4 for Negative unix timestamp***************************");
+		
+		
+		Response res=http.getrequest_futuredate("Base_url1");
+		//Response res1=http.invalid_getrequest("QA_URL",);
+		logger.info("**********Get Request is completed***************");
+	   Responsevalidation.statuscodevalidation(res, 200);
+	   Assert.assertEquals(res.asString(), "false");
       }
-  
- //In this example, the response object is extracted using RestAssured and the timestamp value is retrieved from the response using JSONPath expression. Then, the SimpleDateFormat class is used to parse and format the timestamp value with the desired format. Finally, the parsed timestamp value is compared with the original timestamp value using assertTrue method to assert that the format is correct.
-
- 
-		  
-		  
-		  
-//	import io.restassured.RestAssured;
-//	import io.restassured.response.Response;
-//
-//	public class APIUtilities {
-//
-//	    public static String getUnixTimestamp(String s) {
-//	        String url = "https://helloacm.com/api/unix-timestamp-converter/?cached&s=" + s;
-//	        Response response = RestAssured.get(url);
-//	        String responseBody = response.getBody().asString();
-//	        return responseBody;
-//	    }
-//	}
-
-		 
-
-		
-				
-		
-		
-		
-				
-				//System.out.println("Status code is:"+ res.statusCode());
-				
-				
-				//System.out.println("Data of get request is:"+ res.asString());
-	}
-
 	
 	
+	@Test
+	public void testcase5_get() throws IOException
+	{
+		logger= LogManager.getLogger(this.getClass());
+		logger.info("********** Test case 4 for blank timestamp ***************");
+		Properties pr=LoadProperties.loadproperties1("../APIEpocConverter/URI.Properties");
+		
+		
+		httpmethods http=new httpmethods(pr);
+		System.out.println("******************Test case 5 for Blank unix timestamp***************************");
+		
+		
+		Response res=http.getrequest_futuredate("QA_-veurl");
+		//Response res1=http.invalid_getrequest("QA_URL",);
+		logger.info("**********Get Request is completed***************");
+	   Responsevalidation.statuscodevalidation(res, 200);
+	   Responsevalidation.datevalidation(-1451613802);
+	 		logger.info("**********Date validation is completed***************");
+      }
 	
+	@Test
+	public void testcase6_get() throws IOException
+	{
+		logger= LogManager.getLogger(this.getClass());
+		Properties pr=LoadProperties.loadproperties1("../APIEpocConverter/URI.Properties");
+		
+		
+		httpmethods http=new httpmethods(pr);
+		System.out.println("******************Test case 6 for unix timestamp in microseconds with fractional part***************************");
+		
+		
+		Response res=http.getrequest_futuredate("QA_fractional");
+		//Response res1=http.invalid_getrequest("QA_URL",);
+		logger.info("**********Get Request is completed***************");
+	   Responsevalidation.statuscodevalidation(res, 200);
 
+      }
+	
+	
+	@Test
+	public void testcase7_get() throws IOException
+	{
+		logger= LogManager.getLogger(this.getClass());
+		Properties pr=LoadProperties.loadproperties1("../APIEpocConverter/URI.Properties");
+		
+		
+		httpmethods http=new httpmethods(pr);
+		System.out.println("******************Test case 7 for unix timestamp with very large timestamp***************************");
+		
+		
+		Response res=http.getrequest_futuredate("QA_largetimestamp");
+		logger.info("**********Get Request is completed***************");
+	   Responsevalidation.statuscodevalidation(res, 200);
 
+      }
+	
+}
